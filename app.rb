2a6f499ed43185707441d469ef8a9396f57f8a39
laptite <laptite@gmail.com>
2013-06-30 22:35:06 -0400

    post homework progress

--- app.rb
+++ app.rb
@@ -18,5 +18,21 @@ module Citibike
       erb :home
     end
 
+    get '/form' do
+      erb :form
+    end
+
+    post '/form' do
+      @start = params['start']
+      @end = params['end']
+      erb :form
+    end
+
+    post '/map' do
+      @start = params['start']
+      @end = params['end']
+      erb :map
+    end
+
   end
 end
